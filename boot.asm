7621d5719969fb3f432bf07b1d96410207495f36
3019244289-Li Jinghan <lijinghan@tju.edu.cn>
2021-02-28 18:05:03 +0800

    useless loader

---
+++ boot.asm
@@ -0,0 +1,148 @@
+org 0x7c00
+
+BaseOfStack equ 0x7c00
+BaseOfLoader equ 0x9000
+OffsetOfLoader equ 0x0100
+RootDirSectors equ 14
+SectorNoOfRootDirectory equ 19
+
+
+jmp short START
+nop
+
+BS_OEMName: db "Singular"
+BPB_BytsPerSec: dw 512        ; 每扇区字节数
+BPB_SecPerClus: db 1        ; 每簇多少扇区
+BPB_RsvdSecCnt: dw 1        ; Boot 记录占用多少扇区
+BPB_NumFATs: db 2        ; 共有多少 FAT 表
+BPB_RootEntCnt: dw 224        ; 根目录文件数最大值
+BPB_TotSec16: dw 2880        ; 逻辑扇区总数
+BPB_Media: db 0xF0        ; 媒体描述符
+BPB_FATSz16: dw 9        ; 每FAT扇区数
+BPB_SecPerTrk: dw 18        ; 每磁道扇区数
+BPB_NumHeads: dw 2        ; 磁头数(面数)
+BPB_HiddSec: dd 0        ; 隐藏扇区数
+BPB_TotSec32: dd 0        ; 如果 wTotalSectorCount 是 0 由这个值记录扇区数
+BS_DrvNum: db 0        ; 中断 13 的驱动器号
+BS_Reserved1: db 0        ; 未使用
+BS_BootSig: db 29h        ; 扩展引导标记 (29h)
+BS_VolID: dd 0        ; 卷序列号
+BS_VolLab: db 'Singular.00'; 卷标, 必须 11 个字节
+BS_FileSysType: db 'FAT12   '    ; 文件系统类型, 必须 8个字节 
+
+START:
+	mov ax, cs
+	mov ds, ax
+	mov es, ax
+	mov ss, ax
+	mov sp, BaseOfStack
+
+	mov ax, 0x0600
+	mov bx, 0x0700
+	mov cx, 0
+	mov dx, 0x184f
+	int 0x10
+
+	xor ax, ax
+	xor dl, dl
+	int 0x13
+
+	mov word [wSectorNo], SectorNoOfRootDirectory
+SEARCH_ROOT_DIR:
+	cmp word [wRootDirSizeForLoop], 0
+	jz NO_LOADER
+	dec word [wRootDirSizeForLoop]
+	mov ax, BaseOfLoader
+	mov es, ax
+	mov bx, OffsetOfLoader
+	mov ax, [wSectorNo]
+	mov cl, 1
+	call READ_SECTOR
+	mov si, LoaderFileName
+	mov di, OffsetOfLoader
+	cld
+	mov dx, 16
+SEARCH_FILE:
+	cmp dx, 0
+	jz NEXT_SECTOR
+	dec dx
+	mov cx, 11
+CMP_FILENAME:
+	cmp cx, 0
+	jz FILENAME_FOUND
+	dec cx
+	lodsb
+	cmp al, byte [es:di]
+	jz GO_ON_CMP
+	jmp DIFFERENT
+GO_ON_CMP:
+	inc di
+	jmp CMP_FILENAME
+DIFFERENT:
+	and di, 0xffe0
+	add di, 0x20
+	mov si, LoaderFileName
+	jmp SEARCH_FILE
+NEXT_SECTOR:
+	add word [wSectorNo], 1
+	jmp SEARCH_ROOT_DIR
+NO_LOADER:
+	mov ax, MassageNoLoader
+	mov cx, 10
+	call DISPLAY
+	jmp $
+FILENAME_FOUND:
+	mov ax, MassageLoader
+	mov cx, 14
+	call DISPLAY
+	jmp $
+
+READ_SECTOR:
+	push bp
+	mov bp, sp
+	sub esp, 2
+	mov byte [bp - 2], cl
+	push bx
+	mov bl, [BPB_SecPerTrk]
+	div bl
+	inc ah
+	mov cl, ah
+	mov dh, al
+	shr al, 1
+	mov ch, al
+	and dh, 1
+	pop bx
+	mov dl, [BS_DrvNum]
+READING:
+	mov ah, 2
+	mov al, byte [bp - 2]
+	int 0x13
+	jc READING
+	add esp, 2
+	pop bp
+	ret
+
+DISPLAY:
+	push bp
+	mov bp, ax
+	mov ax, ds
+	mov es, ax
+	mov ax, 0x1301
+	mov bx, 0x7
+	mov dl, 0
+	int 0x10
+	pop bp
+	ret
+
+wRootDirSizeForLoop: dw RootDirSectors
+wSectorNo: dw 0
+bodd: db 0
+
+LoaderFileName: db "LOADER  BIN", 0
+
+MassageBooting: db "Booting"
+MassageLoader: db "Now in Loader!"
+MassageNoLoader: db "No Loader!"
+
+times 510 - ($ - $$) db 0
+dw 0xaa55
---
+++ makefile
@@ -0,0 +1,24 @@
+all: boot write
+
+boot: boot.asm
+	nasm -f bin boot.asm -o boot.bin
+
+write: boot.bin
+	dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
+
+mount:
+	sudo mount -o loop boot.img ./file
+
+umount:
+	sudo umount ./file
+
+run: all
+	bochs -f bochsrc
+
+recreate:
+	rm boot.img
+	dd if=/dev/zero of=./boot.img bs=512 count=2880
+
+clean:
+	rm boot.bin boot.img
+
